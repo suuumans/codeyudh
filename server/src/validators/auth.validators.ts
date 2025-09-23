82a443cfca9f38ea5cb23869ab7d465271855cf7
Suman Sarkar <152314759+suumans@users.noreply.github.com>
2025-09-23 12:17:38 +0530

    adding username for register user

--- server/src/validators/auth.validators.ts
+++ server/src/validators/auth.validators.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const registerUserSchema = z.object({
     name: z.string().min(3).max(50),
-    // username: z.string().min(3).max(50),
+    username: z.string().min(3).max(50),
     email: z.string().email(),
     password: z.string().min(6).max(50),
 });
@@ -20,4 +20,3 @@ export const forgotPasswordSchema = z.object({
 export const resendVerificationEmailSchema = z.object({
     email: z.string().email(),
 });
-

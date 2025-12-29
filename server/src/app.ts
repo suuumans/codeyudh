d2a6af27596a39e96aaafa081a90480ea2411e29
Suman Sarkar <152314759+suumans@users.noreply.github.com>
2025-12-29 14:41:06 +0530

    feat: Make CORS origin configurable via environment variable.

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ import { functions } from '../src/utils/notification/notification.handler.ts';
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["content-type", "Authorization"]
